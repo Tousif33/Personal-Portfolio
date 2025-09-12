207fbcf55520600ac2d3c576b89824b092a6d51c
Tousif Razagit config --global user.email tousifraza9696@gmail.com <ahsanraza7117@gmail.com>
2025-09-12 18:18:04 +0530

    Add vercel.json for SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+
 
 const root = createRoot(document.getElementById("root"));
 root.render(
 
-    <BrowserRouter>
+    <BrowserRouter basename="/Personal-Portfolio">
    
         <App />
    
