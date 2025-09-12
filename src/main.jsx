18d04c5d051528a7f6bd1978987527fe9b58c307
Tousif Razagit config --global user.email tousifraza9696@gmail.com <ahsanraza7117@gmail.com>
2025-09-12 18:59:10 +0530

    Fixed routing issue for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+const isVercel = import.meta.env.VERCEL === "1";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-
-    <BrowserRouter basename="/Personal-Portfolio">
-   
-        <App />
-   
-    </BrowserRouter>
+  <BrowserRouter basename={isVercel ? "/" : "/Personal-Portfolio/"}>
+    <App />
+  </BrowserRouter>
 );
