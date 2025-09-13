85505ba239f2c1ff067fd2d10c2fe64d55c283aa
Tousif Razagit config --global user.email tousifraza9696@gmail.com <ahsanraza7117@gmail.com>
2025-09-13 11:31:11 +0530

    Updated vite.config with base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", // ✅ Important for Vercel
+  base: "/Personal-Portfolio/", // 👈 must match repo name
 })
