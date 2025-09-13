fac8ca78e642a6e5327e3861f4c306f9d7747a6b
Tousif Razagit config --global user.email tousifraza9696@gmail.com <ahsanraza7117@gmail.com>
2025-09-13 11:51:46 +0530

    Setup GitHub Pages deployment and fixed Resume links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// app.jsx
 import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
@@ -8,23 +7,24 @@ import Resume from "./Resume";
 import Footer from "./Footer";
 
 export default function App() {
-     
-  return(
-     <>
-      {/* Content */}
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar stays on top */}
       <Navbar />
 
-      <Routes>
-        {/* Redirect / to /home */}
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
+      {/* Main content grows to push footer down */}
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </main>
 
+      {/* Footer stays at bottom */}
       <Footer />
-    
-    </>
+    </div>
   );
 }
